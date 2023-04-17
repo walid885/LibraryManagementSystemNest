e9ed99bba67cfcb45f349043941806a5e8e346c6
walid885 <bettaieb88walid@gmail.com>
2023-04-17 22:47:04 +0100

    installing prisma client and library + simple functionality of sign in and sign up

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,17 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { UserModule } from './user/user.module';
-import { ModuleController } from './serivce/module/module.controller';
 import { BookmarkModule } from './bookmark/bookmark.module';
 import { AuthController } from './auth/auth.controller';
 import { AuthModule } from './auth/auth.module';
 import { AuthService } from './auth/auth.service';
-import { AuthController } from './auth/auth.controller';
 
 @Module({
-  imports: [UserModule, BookmarkModule, AuthModule],
-  controllers: [AppController, ModuleController, AuthController],
+  imports: [ BookmarkModule, AuthModule],
+  controllers: [AppController,  AuthController],
   providers: [AppService, AuthService],
 })
 export class AppModule {}
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,15 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+    signup(){
+        return {
+            msg : ' I have signed up '
+        }
+    }
+    signin(){
+        return {
+            msg : ' I have signed In '
+        }
+    }
+}
